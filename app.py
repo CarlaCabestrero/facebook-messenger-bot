834973a669e4e11ed57262e234e5b699548744d7
Carla Cabestrero <carla.cabestrero@mo2o.com>
2018-03-19 02:18:43 +0100

    rodoso funciona

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import os, sys
 from flask import Flask, request
-from utils import wit_response, get_news_elements
+from utils import wit_response, get_response_message, get_translated_action, get_news_elements
 from pymessenger import Bot
 
 app = Flask(__name__)
+one = 0
+jamala = '1557317664386664'
+bot_id = '897695543743171'
 
 
+PAGE_ACCESS_TOKEN = "EAAEGgHE2HqsBACcxBhVaejIKhe9ovXabfs6842IuAgz5w5lgjZAdOJNL2zB5lOySFDszCZAHOQfULiyvjXrg8S8MeOWZAJuMWpYz1QcRKmZA6L5arp7i7MysEdKzu8vWQsCLQs8UYsN62jtZCTYs8tavZBjdRB313jFFVGAlliQQZDZD"
 
-PAGE_ACCESS_TOKEN = " "
 
 bot = Bot(PAGE_ACCESS_TOKEN)
 
 
-
-@app.route('/', methods =['GET'])
+@app.route('/', methods=['GET'])
 def verify():
-
-    if request.args.get("hub.mode") == "subscribe" and            request.args.get("hub.challenge"):
-        if not request.args.get("hub.challenge"):
+    # Webhook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
     return "Hello world", 200
-    
-    
+
+
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
@@ -31,43 +33,95 @@ def webhook():
     if data['object'] == 'page':
         for entry in data['entry']:
             for messaging_event in entry['messaging']:
-                
-                #IDs
-                sender_id = messaging_event['sender']['id']
-                recipient_id = messaging_event['recipient']['id']
-                
-                #Check message type
-                if messaging_event.get('message'):
-                    if 'text' in messaging_event['message']:
-                        messaging_text  = messaging_event['message']['text']
-                    else:
-                        messaging_text = 'no text'
+                # IDs
+                    sender_id = messaging_event['sender']['id']
+                    recipient_id = messaging_event['recipient']['id']
+                    if messaging_event.get('message'):
+                        if sender_id == jamala or recipient_id == jamala or sender_id == bot_id:
+                            pass
+                        else:
+                            # Extracting text message
+                            respond(messaging_event, sender_id, recipient_id, jamala)
+    return 'ok', 200
+                       
+                            
+def respond(messaging_event, sender_id, recipient_id, jamala):
 
-                categories = wit_response(messaging_text)
+    if sender_id == jamala or recipient_id == jamala or sender_id == bot_id:
+        pass
+    else:
+        if 'text' in messaging_event['message']:
+            messaging_text = messaging_event['message']['text']
+        else:
+            messaging_text = 'no text'
 
-                elements = get_news_elements(categories)
-                bot.send_generic_message(sender_id, elements)
-                    # #Echo
-                    # response = None
+        categories = wit_response(messaging_text)
+        response_messages = get_response_message(categories)
+        actions = get_translated_action(categories)
+        print('aqui van')
+        print(response_messages)
+        print(actions)
 
-                    # entity, value = wit_response(messaging_text)
+        for response_message, action in zip(response_messages, actions):
+            if response_message == 'saludo':
+                if(action == 'adios'):
+                    bot.send_text_message(sender_id, 'Hasta la vista!')
+                else:
+                    bot.send_text_message(sender_id, '¡Hola!')
 
-                    # if entity == 'newstype':
-                    # 	response = "Ok I will send you {} news".format(str(value))
-                    # elif entity == "location":
-                    # 	response = "Ok. So, you live in {0}. I will send you top headlines from {0}".format(str(value))
+                bot.send_text_message(jamala, 'wave left hand')
+
+            if response_message == 'quetal':
+                bot.send_text_message(sender_id, 'Yo estoy muy bien, ¿y tu?')
+                bot.send_text_message(jamala, action)
+
+
+            if response_message == 'message':
+                respond(categories['message'])
+
+            if response_message == 'rodoso':
+                pass  
+
+            if response_message == 'agradecimiento':
+
+                bot.send_text_message(sender_id, 'De nada.')
+                bot.send_text_message(jamala, action) 
+
+            if(response_message == 'deporte'):
+                    bot.send_text_message(sender_id, 'Soy el mejor Karateka.')
+                    bot.send_text_message(jamala, 'Left kick')
                     
-                    # if response == None :
-                    #     response = "Sorry I didn't understand!"
+            if(response_message == 'peticion'):
+                    bot.send_text_message(sender_id, '¡Claro!')
 
-                    # bot.send_text_message(sender_id, response) 
 
+            if response_message == 'orden':
+                if(action == 'sientate'):
+                    bot.send_text_message(sender_id, 'Sentadito me quedé.')
+                    bot.send_text_message(jamala, 'sit')
+                if(action == 'baila'):
+                    bot.send_text_message(sender_id, '¡Que alguien suba la música!')
+                    bot.send_text_message(jamala, 'dance')
+                if(action == 'arriba'):
+                    bot.send_text_message(sender_id, '¡Arriba!')
+                    bot.send_text_message(jamala, 'stand up')
+                if(action == 'ejercicio'):
+                    bot.send_text_message(sender_id, 'Esto no me cuesta nada.')
+                    bot.send_text_message(jamala, 'push up')
+
+
+
+        #elements = get_news_elements(categories)
+        #print(type(sender_id))
 
     return "ok", 200
-    
+
+
 def log(message):
     print(message)
     sys.stdout.flush()
-    
+
+
 if __name__ == "__main__":
-    app.run(debug= True, port = 80)
+    app.run(debug = True, port = 80)
+
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 from wit import Wit
 from gnewsclient import gnewsclient
 
-access_token = "6WHOSJPFPMFLSL4PQWVPL5RHWATOGZ7Q"
+access_token = "KZK5THS2X4KO4C5OWJFGHSFAEC2Z6GBO"
 
 client = Wit(access_token = access_token)
 
 def wit_response(message_text):
 	resp = client.message(message_text)
-	categories = {'newstype':None, 'location':None}
+	categories = {'saludo':None, 'quetal':None, 'message':None, 'rodoso':None, 'orden':None, 'agradecimiento':None, 'deporte':None, 'peticion':None}
 
 	entities=list(resp['entities'])
 
@@ -15,6 +15,56 @@ def wit_response(message_text):
 		categories[entity] = resp['entities'][entity][0]['value']
 
 	return categories
+
+def get_response_message(categories):
+	message = []
+	if categories['saludo'] != None:
+		message.append('saludo')
+
+	if categories['quetal'] != None:
+		message.append('quetal')
+
+	if categories['message'] != None:
+		message.append('message')
+
+	if categories['orden'] != None:
+		message.append('orden')
+
+	if categories['agradecimiento'] != None:
+		message.append('agradecimiento')
+
+	if categories['deporte'] != None:
+		message.append('deporte')
+
+	if categories['peticion'] != None:
+		message.append('peticion')
+
+	return message
+
+
+def get_translated_action(categories):
+	actions = []
+	if categories['saludo'] != None:
+		actions.append(categories['orden'])
+
+	if categories['quetal'] != None:
+		actions.append('greet')
+
+	if categories['orden'] != None:
+		actions.append(categories['orden'])
+
+	if categories['agradecimiento'] != None:
+		actions.append('greet')
+
+	if categories['deporte'] != None:
+		actions.append('deporte')
+
+	if categories['peticion'] != None:
+		actions.append('peticion')
+
+
+	return actions
+
 
 def get_news_elements(categories):
 	news_client = gnewsclient()
@@ -28,21 +78,24 @@ def get_news_elements(categories):
 		news_client.query += categories['newstype'] + ' '
 
 
+
 	news_items = news_client.get_news()
 	elements = []
 
-
-	for item in news_items:
-		element = {
-					'title': item['title'],
-					'buttons': [{
-								'type': 'web_url',
-								'title': "Read more",
-								'url': item['link']
-					}],
-					'image_url': item['img']
-		}
-		elements.append(element)
+	if news_items is NoneType:
+		return elements.append('naita')
+	else:
+		for item in news_items:
+			element = {
+						'title': item['title'],
+						'buttons': [{
+									'type': 'web_url',
+									'title': "Read more",
+									'url': item['link']
+						}],
+						'image_url': item['img']
+			}
+			elements.append(element)
 
 	return elements
 
